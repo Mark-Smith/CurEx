788cde12d6937d8a87611ee3221fe28b5c8fa6b7
markgsmith64@gmail.com <msmith@intimemedia.com>
2016-08-26 07:34:20 +0100

    Initial Commit

---
+++ CurEx/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurEx
+//
+//  Created by Mark Smith on 26/08/2016.
+//  Copyright © 2016 ___CurEx___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
